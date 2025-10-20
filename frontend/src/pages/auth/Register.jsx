b4896ed558a27965ee9552f94f4ef61d219d3a79
Aayush <shahayush688@gmail.com>
2025-10-20 09:45:47 +0530

    register comonent changes has been made

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -151,7 +151,6 @@ const Register = () => {
               <option value="User">User</option>
             </select>
 
-
             {/* Submit */}
             <button
               type="submit"
