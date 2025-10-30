f8891fc2ea1c58f9547e2852923eb2f67030a4b5
Aayush <shahayush688@gmail.com>
2025-10-30 16:43:41 +0530

    frontend project file has been updated

---
+++ frontend/src/pages/Sample.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  FiPlus,
+  FiMoreHorizontal,
+  FiAlignLeft,
+  FiX,
+  FiActivity,
+  FiUser,
+  FiCalendar,
+  FiTag,
+  FiCheckSquare,
+  FiLink,
+  FiImage,
+  FiBold,
+  FiItalic,
+} from "react-icons/fi";
+
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+
+// taskDetailModel Components --- 
+
+const TaskDetailModal = ({task, listName, onClose, onUpdateTask }) =>{
+    return(
+        <>
+        <div
+            className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-start pt-10 z-50 overflow-y-auto"
+        >
+
+        </div>
+        </>
+    )
+}
